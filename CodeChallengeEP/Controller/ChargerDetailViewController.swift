8dd2c8887c53281a5cfb7a87d7878a8c2ecd60a8
SammyCalle <sammycallet@gmail.com>
2024-03-14 15:27:30 +0200

    2/3 bonush challenge done

---
+++ CodeChallengeEP/Controller/ChargerDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChargerDetailView.swift
+//  CodeChallengeEP
+//
+//  Created by Sammy Alonso Calle Torres on 3/12/24.
+//
+
+import UIKit
+
+class ChargerDetailViewController: UIViewController {
+    
+    var charger : Charger?
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+
+}
